c712d34f09f39a009fd08a069444d9df6e955b0b
unknown <thiagosequeira02@gmail.com>
2021-12-02 14:25:11 -0300

    fix: bug main.js

--- components/layouts/main.js
+++ components/layouts/main.js
@@ -17,8 +17,13 @@ const Main = ({ children, router }) => {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="description" content="Thiago Sequeira homepage" />
         <meta name="author" content="Thiago Sequeira" />
+        <meta name="author" content="thiagosequeira" />
         <link rel="apple-touch-icon" href="apple-touch-icon.png" />
         <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="@thiagosequeira" />
+        <meta name="twitter:creator" content="@thiagosequeira" />
+        <meta name="twitter:image" content="/card.png" />
         <meta property="og:site_name" content="Thiago Sequeira Homepage" />
         <meta property="og:type" content="website" />
         <meta property="og:image" content="/card.png" />
